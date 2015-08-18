22abb68bf867bc065715882eb6412be5f7098160
spareluck <jonjardim@gmail.com>
2015-08-18 15:48:10 -0700

    hadouken animation and sound added

--- js/app.js
+++ js/app.js
@@ -8,16 +8,31 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		console.log('mousedown');
 	 	// play hadouken sound
+	 	playHadouken();
 	 	$('.ryu-ready').hide();
 	 	$('.ryu-throwing').show();
-	 	$('.hadouken').show();
 	 	// show hadouken and animate it to right of screen
+	 	$('.hadouken').finish().show()
+	 		.animate(
+				{'left': '1020px'},
+				500,
+				function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+				}
+			);
 	})
 	.mouseup(function() {
 	    // ryu goes back to his ready position
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
